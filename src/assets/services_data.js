3b3a1d7f69a5fb4baa34682490fa697b01b3e641
Lakhan Raut <lakhanraut709@gmail.com>
2025-09-14 07:49:55 +0530

    Service-Update

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -7,27 +7,27 @@ const Services_Data = [
     {
         s_no:"02",
         s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Graphics design is the process of creating visual content, combining typography..."
     },
     {
         s_no:"03",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Social media management is the process of creating, scheduling, analyzing, and..."
     },
     {
         s_no:"04",
         s_name:"Data Analysis",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Data analysis is the process of collecting, cleaning, interpreting, and visualizing..."
     },
     {
         s_no:"05",
         s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Digital marketing is the process of promoting products or services using digital..."
     },
     {
         s_no:"06",
         s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Content writing is the process of planning, creating, and optimizing..."
     },
 ]
  
